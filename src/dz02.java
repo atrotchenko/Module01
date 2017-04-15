103738bbffd0bc4668b909aec1925277025b4b1f
atrotchenko <andrey.trotchenko@gmail.com>
2017-04-15 18:44:48 +0300

    Домашнее задание №2

---
+++ src/dz02.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * Created by victorpetrov on 13.04.17.
+ */
+public class dz02 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter number 1: ");
+        int a = Integer.parseInt(reader.readLine());
+        System.out.println("Enter number 2: ");
+        int b = Integer.parseInt(reader.readLine());
+        System.out.println("Enter number 3: ");
+        int c = Integer.parseInt(reader.readLine());
+        System.out.println("Enter number 4: ");
+        int d = Integer.parseInt(reader.readLine());
+        System.out.println("Enter number 5:");
+        int e = Integer.parseInt(reader.readLine());
+
+        int [] arr = {a,b,c,d,e};
+
+        System.out.println("Initial array: " + Arrays.toString(arr));
+
+        Arrays.sort(arr);
+
+        System.out.print("Sorted array: ");
+
+
+        System.out.print(arr[0] + " ");
+        System.out.print(arr[1] + " ");
+        System.out.print(arr[2] + " ");
+        System.out.print(arr[3] + " ");
+        System.out.print(arr[4] + " ");
+    }
+
+
+}
+
