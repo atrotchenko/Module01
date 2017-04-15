98ecb8eb0f6417259fdff0b36f8c2ba5dba09f8e
atrotchenko <andrey.trotchenko@gmail.com>
2017-04-15 18:48:48 +0300

    Домашнее задание №2

--- src/dz02.java
+++ src/dz02.java
@@ -7,6 +7,29 @@ import java.util.Arrays;
  */
 public class dz02 {
     public static void main(String[] args) throws Exception {
+        /* Trotchenko
+           Создать консольное приложение, которое просит пользователя ввести из консоли 5 чисел
+Все числа должны быть записаны в массив целых чисел.
+После этого, массив должен быть отсортирован по возрастанию и выведен консоль.
+Сначала начальная версия, далее - отсортированная.
+
+Пример:
+
+Enter number 1:
+5
+Enter number 2:
+4
+Enter number 3:
+3
+Enter number 4:
+2
+Enter number 5:
+1
+
+Initial array: 5, 4, 3, 2, 1
+Sorted array: 1, 2, 3, 4, 5.
+
+        */
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
