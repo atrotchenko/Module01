56193af9c21ecdcfad11d029197ea0a7a4c914a0
atrotchenko <andrey.trotchenko@gmail.com>
2017-04-15 18:52:27 +0300

    Домашнее задание №2

--- src/dz01.java
+++ src/dz01.java
@@ -2,12 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Created by victorpetrov on 13.04.17.
- */
 public class dz01 {
     public static void main(String[] args) throws IOException {
+    /*    Trotchenko
+Создать консольное приложение, которое запрашивает имя пользователя. После ввода имени выводит строку: Hello, <введённое имя>
+Пример:
 
+Enter Your name:
+Eugene
+
+Hello, Eugene */
         System.out.println("Enter You name: ");
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
